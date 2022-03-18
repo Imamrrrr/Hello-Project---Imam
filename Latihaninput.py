b58f7f27e3db7a10f773ca05b26e27263f9ebce5
Imamrrrr <imamrifaii08@gmail.com>
2022-03-18 11:10:53 +0800

    file python

---
+++ Latihaninput.py
@@ -0,0 +1,15 @@
+"""
+a, b = map(int, input().split())
+print('+',a + b)
+print('-',a - b)
+print('*',a * b)
+print('/',a / b)
+print('//',a // b)
+"""
+
+"""
+a, b, c, d = map(int,input().split())
+print((a/b)==(c/d))
+
+"""
+
